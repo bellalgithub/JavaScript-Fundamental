862ee5b5811dd1a045b79ba1fd7eeb32d2aed801
Bellal <bellalsq@gmail.com>
2022-12-08 22:36:30 +0600

    Array updated

--- array.js
+++ array.js
@@ -45,4 +45,83 @@ Output:
 -1 */
 
 
+// Array Element add from the end
+
+const devs = ['Bellal', 'Noyon', 'Nazmul', 'Sakib'];
+
+devs.push('Iqbal');
+devs.push('Faruk');
+
+// console.log(devs);
+
+/* Output: 
+
+[ 'Bellal', 'Noyon', 'Nazmul', 'Sakib', 'Iqbal', 'Faruk' ]
+ */
+
+
+// Array Element remove from the end
+
+devs.pop();
+
+/* console.log(devs);
+
+Output: 
+[ 'Bellal', 'Noyon', 'Nazmul', 'Sakib', 'Iqbal' ]
+
+ */
+
+// Array Element add from the start
+
+
+devs.unshift('Asma');
+
+/* console.log(devs);
+
+Output:
+[ 'Asma', 'Bellal', 'Noyon', 'Nazmul', 'Sakib', 'Iqbal' ] */
+
+
+// Array Element remove from the start
+
+devs.shift();
+
+/* console.log(devs);
+
+Output:
+
+[ 'Bellal', 'Noyon', 'Nazmul', 'Sakib', 'Iqbal' ]
+
+
+ */
+
+// More than onbe elements add from the end
+
+// console.log(devs);
+devs.push('Hasan', 'Hossain');
+/* console.log(devs);
+Output:
+[
+    'Bellal',  'Noyon',
+    'Nazmul',  'Sakib',
+    'Iqbal',   'Hasan',
+    'Hossain'
+  ] */
+
+
+// More than onbe elements add from the start
+
+devs.unshift('Rahim', 'Karim')
+/* console.log(devs);
+
+Output:
+
+[
+    'Rahim',   'Karim',
+    'Bellal',  'Noyon',
+    'Nazmul',  'Sakib',
+    'Iqbal',   'Hasan',
+    'Hossain'
+  ] */
+
 
