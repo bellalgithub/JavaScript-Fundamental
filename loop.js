2d12bc45b06c096465d5311b3f8dff7c321d8149
Bellal <bellalsq@gmail.com>
2022-12-09 21:45:09 +0600

    For lopp updATED

--- loop.js
+++ loop.js
@@ -16,9 +16,77 @@ while (price <= 210) {
 202
 203
 204
-205
+205git 
 206
 207
 208
 209
 210 */
+
+// ------------------For Loop -----------------------------------
+
+let name = 'Bellal Hossain'
+for (let i = 0; i < 5; i++) {
+
+    // console.log(name);
+}
+
+/* Output:
+
+Bellal Hossain
+Bellal Hossain
+Bellal Hossain
+Bellal Hossain
+Bellal Hossain */
+
+const employee = ['Bellal', 'Noyon', 'Sakib', 'Nazmul', 'Nahid'];
+for (let i = 0; i < employee.length; i++) {
+
+    /*     console.log(' -----------------------------------------');
+        console.log(employee)
+    
+        console.log(employee[3])
+        console.log(' -----------------------------------------'); */
+
+}
+
+/* Output:
+ -----------------------------------------
+[ 'Bellal', 'Noyon', 'Sakib', 'Nazmul', 'Nahid' ]
+Nazmul
+ -----------------------------------------
+ -----------------------------------------
+[ 'Bellal', 'Noyon', 'Sakib', 'Nazmul', 'Nahid' ]
+Nazmul
+ -----------------------------------------
+ -----------------------------------------
+[ 'Bellal', 'Noyon', 'Sakib', 'Nazmul', 'Nahid' ]
+Nazmul
+ -----------------------------------------
+ -----------------------------------------
+[ 'Bellal', 'Noyon', 'Sakib', 'Nazmul', 'Nahid' ]
+Nazmul
+ -----------------------------------------
+ -----------------------------------------
+[ 'Bellal', 'Noyon', 'Sakib', 'Nazmul', 'Nahid' ]
+Nazmul
+ ----------------------------------------- */
+
+
+const priceAmount = [44, 55, 66, 77, 88, 11, 22, 33];
+
+for (let i = 0; i < priceAmount.length; i++) {
+
+    // console.log(priceAmount[i]);
+}
+
+/* Output:
+
+44
+55
+66
+77
+88
+11
+22
+33 */
